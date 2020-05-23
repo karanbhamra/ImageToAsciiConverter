e3339dfcdf65d1b56e56877cf965e91b2874a49d
Karan Bhamra <karan.bhamra@gmail.com>
2020-05-23 14:10:45 -0700

    initial upload

---
+++ ImageToAscii/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ImageToAscii
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseImageButton = new System.Windows.Forms.Button();
+            this.initialPictureBox = new System.Windows.Forms.PictureBox();
+            this.asciiPictureBox = new System.Windows.Forms.PictureBox();
+            this.saveImageButton = new System.Windows.Forms.Button();
+            this.convertImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.initialPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asciiPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // browseImageButton
+            // 
+            this.browseImageButton.Location = new System.Drawing.Point(146, 12);
+            this.browseImageButton.Name = "browseImageButton";
+            this.browseImageButton.Size = new System.Drawing.Size(170, 29);
+            this.browseImageButton.TabIndex = 0;
+            this.browseImageButton.Text = "Browse Image";
+            this.browseImageButton.UseVisualStyleBackColor = true;
+            this.browseImageButton.Click += new System.EventHandler(this.browseImageButton_Click);
+            // 
+            // initialPictureBox
+            // 
+            this.initialPictureBox.Location = new System.Drawing.Point(13, 63);
+            this.initialPictureBox.Name = "initialPictureBox";
+            this.initialPictureBox.Size = new System.Drawing.Size(481, 471);
+            this.initialPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.initialPictureBox.TabIndex = 1;
+            this.initialPictureBox.TabStop = false;
+            // 
+            // asciiPictureBox
+            // 
+            this.asciiPictureBox.Location = new System.Drawing.Point(550, 63);
+            this.asciiPictureBox.Name = "asciiPictureBox";
+            this.asciiPictureBox.Size = new System.Drawing.Size(481, 471);
+            this.asciiPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asciiPictureBox.TabIndex = 1;
+            this.asciiPictureBox.TabStop = false;
+            // 
+            // saveImageButton
+            // 
+            this.saveImageButton.Location = new System.Drawing.Point(742, 12);
+            this.saveImageButton.Name = "saveImageButton";
+            this.saveImageButton.Size = new System.Drawing.Size(128, 29);
+            this.saveImageButton.TabIndex = 0;
+            this.saveImageButton.Text = "Save Image";
+            this.saveImageButton.UseVisualStyleBackColor = true;
+            this.saveImageButton.Click += new System.EventHandler(this.saveImageButton_Click);
+            // 
+            // convertImageButton
+            // 
+            this.convertImageButton.Location = new System.Drawing.Point(435, 12);
+            this.convertImageButton.Name = "convertImageButton";
+            this.convertImageButton.Size = new System.Drawing.Size(170, 29);
+            this.convertImageButton.TabIndex = 0;
+            this.convertImageButton.Text = "Convert Image";
+            this.convertImageButton.UseVisualStyleBackColor = true;
+            this.convertImageButton.Click += new System.EventHandler(this.convertImageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 546);
+            this.Controls.Add(this.convertImageButton);
+            this.Controls.Add(this.saveImageButton);
+            this.Controls.Add(this.asciiPictureBox);
+            this.Controls.Add(this.initialPictureBox);
+            this.Controls.Add(this.browseImageButton);
+            this.Name = "Form1";
+            this.Text = "ImageToAscii Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.initialPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asciiPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseImageButton;
+        private System.Windows.Forms.PictureBox initialPictureBox;
+        private System.Windows.Forms.PictureBox asciiPictureBox;
+        private System.Windows.Forms.Button saveImageButton;
+        private System.Windows.Forms.Button convertImageButton;
+    }
+}
+
---
+++ ImageToAscii/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToAscii
+{
+    public partial class Form1 : Form
+    {
+        Bitmap initialImage;
+        string initialText;
+
+        string brightness = "`^\\\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            asciiPictureBox.Click += pictureBox_Clicked;
+            initialPictureBox.Click += pictureBox_Clicked;
+        }
+
+        private void pictureBox_Clicked(object sender, EventArgs e)
+        {
+            // to do, open new form with larger version of the image
+            PictureBox pbox = sender as PictureBox;
+
+            if (pbox.Image is null)
+            {
+                return;
+            }
+
+            Form form = new Form();
+            form.Text = "Preview";
+            form.Size = pbox.Image.Size;
+            form.WindowState = FormWindowState.Maximized;
+
+            var temp = new PictureBox();
+            temp.Click += (obj, args) => form.Close();
+            temp.Size = form.Size;
+            temp.Image = pbox.Image;
+            temp.SizeMode = PictureBoxSizeMode.StretchImage;
+            form.Controls.Add(temp);
+
+            form.Show();
+        }
+
+        private void browseImageButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    string filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    bitmap = new Bitmap(filePath);
+                    initialImage = bitmap;
+                    initialPictureBox.Image = initialImage;
+                }
+            }
+
+        }
+
+        private async void convertImageButton_Click(object sender, EventArgs e)
+        {
+            if (initialImage == null)
+            {
+                MessageBox.Show("Please select an image first.");
+                return;
+            }
+
+            var image = await ConvertImageAsync(initialImage);
+
+            asciiPictureBox.Image = image.bitmap;
+            initialText = image.str;
+        }
+
+        private Task<(Bitmap bitmap, string str)> ConvertImageAsync(Bitmap image)
+        {
+            return Task.Factory.StartNew(() => ConvertImage(image));
+        }
+
+        private (Bitmap bitmap, String str) ConvertImage(Bitmap image)
+        {
+            Bitmap bitmap = new Bitmap(image.Width * 15, image.Height * 15);
+            Graphics gfx = Graphics.FromImage(bitmap);
+            Font font = new Font("Arial", 12);
+            gfx.Clear(Color.White);
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var orig = image.GetPixel(x, y);
+
+                    var newchar = ColorToCharacter(orig).ToString();
+                    sb.Append(newchar);
+
+                    gfx.DrawString(newchar, font, Brushes.Black, x * 15, y * 15);
+
+                }
+                sb.Append(Environment.NewLine);
+            }
+            return (bitmap, sb.ToString());
+        }
+
+        private async void saveImageButton_Click(object sender, EventArgs e)
+        {
+            if (initialImage == null)
+            {
+                MessageBox.Show("Please select an image first.");
+                return;
+            }
+
+            var path = Directory.GetCurrentDirectory();
+            var file = path + Guid.NewGuid().ToString();
+
+            asciiPictureBox.Image.Save(file + ".jpg");
+            await File.WriteAllTextAsync(file + ".txt", initialText);
+
+            MessageBox.Show($"Saved to {path}");
+
+        }
+
+        public char ColorToCharacter(Color pix)
+        {
+            int tempBrightness = (int)Math.Floor(pix.GetBrightness() * 256);
+
+            int index = tempBrightness % brightness.Length;
+
+            return brightness[index];
+        }
+    }
+}
